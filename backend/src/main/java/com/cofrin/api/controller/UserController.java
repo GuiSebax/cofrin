da1fa530ba2b03b42884a07e6f616c78530f53de
GuiSebax <guiclemente2003@hotmail.com>
2026-09-05 17:41:25 -0300

    feat: add user creation endpoint

---
+++ backend/src/main/java/com/cofrin/api/controller/UserController.java
@@ -0,0 +1,29 @@
+package com.cofrin.api.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cofrin.api.entity.User;
+import com.cofrin.api.service.UserService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/users")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        User created = userService.create(user);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(created);
+    }
+
+}
